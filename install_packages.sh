c4e1a43ab85e09516a1865402b217a90e6344cd9
Haochen Zhang <hz112@rice.edu>
2026-02-07 16:59:14 -0600

    0207

---
+++ install_packages.sh
@@ -0,0 +1,4 @@
+pip install wandb
+pip install seaborn
+pip install omegaconf
+pip install torchaudio==2.0.0
--- utils/utils_data.py
+++ utils/utils_data.py
@@ -11,118 +11,125 @@
 # sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 #
 #
-# def MinMaxScaler(data, return_scalers=False):
-#     """Min Max normalizer.
-#
-#     Args:
-#       - data: original data
-#
-#     Returns:
-#       - norm_data: normalized data
-#     """
-#     min = np.min(data, 0)
-#     max = np.max(data, 0)
-#     numerator = data - np.min(data, 0)
-#     denominator = np.max(data, 0) - np.min(data, 0)
-#     norm_data = numerator / (denominator + 1e-7)
-#     if return_scalers:
-#         return norm_data, min, max
-#     return norm_data
-#
-#
-# def MinMaxArgs(data, min, max):
-#     """
-#     Args:
-#         data: given data
-#         min: given min value
-#         max: given max value
-#
-#     Returns:
-#         min-max scaled data by given min and max
-#     """
-#     numerator = data - min
-#     denominator = max - min
-#     norm_data = numerator / (denominator + 1e-7)
-#     return norm_data
-#
-#
-# def sine_data_generation(no, seq_len, dim):
-#     """Sine data generation.
-#
-#     Args:
-#       - no: the number of samples
-#       - seq_len: sequence length of the time-series
-#       - dim: feature dimensions
-#
-#     Returns:
-#       - data: generated data
-#     """
-#     # Initialize the output
-#     data = list()
-#
-#     # Generate sine data
-#     for i in range(no):
-#         # Initialize each time-series
-#         temp = list()
-#         # For each feature
-#         for k in range(dim):
-#             # Randomly drawn frequency and phase
-#             freq = np.random.uniform(0, 0.1)
-#             phase = np.random.uniform(0, 0.1)
-#
-#             # Generate sine signal based on the drawn frequency and phase
-#             temp_data = [np.sin(freq * j + phase) for j in range(seq_len)]
-#             temp.append(temp_data)
-#
-#         # Align row/column
-#         temp = np.transpose(np.asarray(temp))
-#         # Normalize to [0,1]
-#         temp = (temp + 1) * 0.5
-#         # Stack the generated data
-#         data.append(temp)
-#
-#     return data
-#
-#
-# def real_data_loading(data_name, seq_len):
-#     """Load and preprocess real-world data.
-#
-#     Args:
-#       - data_name: stock or energy
-#       - seq_len: sequence length
-#
-#     Returns:
-#       - data: preprocessed data.
-#     """
-#     assert data_name in ['stock', 'energy', 'metro']
-#
-#     if data_name == 'stock':
-#         ori_data = np.loadtxt('./data/short_range/stock_data.csv', delimiter=",", skiprows=1)
-#     elif data_name == 'energy':
-#         ori_data = np.loadtxt('./data/short_range/energy_data.csv', delimiter=",", skiprows=1)
-#     elif data_name == 'metro':
-#         ori_data = np.loadtxt('./data/short_range/metro_data.csv', delimiter=",", skiprows=1)
-#
-#     # Flip the data to make chronological data
-#     ori_data = ori_data[::-1]
-#     # Normalize the data
-#     ori_data = MinMaxScaler(ori_data)
-#
-#     # Preprocess the data
-#     temp_data = []
-#     # Cut data by sequence length
-#     for i in range(0, len(ori_data) - seq_len):
-#         _x = ori_data[i:i + seq_len]
-#         temp_data.append(_x)
-#
-#     # Mix the data (to make it similar to i.i.d)
-#     idx = np.random.permutation(len(temp_data))
-#     data = []
-#     for i in range(len(temp_data)):
-#         data.append(temp_data[idx[i]])
-#
-#     return data
-#
+import numpy as np
+from torch.utils.data import DataLoader, Dataset, Subset
+import torch
+import importlib
+from torch.utils.data import ConcatDataset
+
+
+def MinMaxScaler(data, return_scalers=False):
+    """Min Max normalizer.
+
+    Args:
+      - data: original data
+
+    Returns:
+      - norm_data: normalized data
+    """
+    min = np.min(data, 0)
+    max = np.max(data, 0)
+    numerator = data - np.min(data, 0)
+    denominator = np.max(data, 0) - np.min(data, 0)
+    norm_data = numerator / (denominator + 1e-7)
+    if return_scalers:
+        return norm_data, min, max
+    return norm_data
+
+
+def MinMaxArgs(data, min, max):
+    """
+    Args:
+        data: given data
+        min: given min value
+        max: given max value
+
+    Returns:
+        min-max scaled data by given min and max
+    """
+    numerator = data - min
+    denominator = max - min
+    norm_data = numerator / (denominator + 1e-7)
+    return norm_data
+
+
+def sine_data_generation(no, seq_len, dim):
+    """Sine data generation.
+
+    Args:
+      - no: the number of samples
+      - seq_len: sequence length of the time-series
+      - dim: feature dimensions
+
+    Returns:
+      - data: generated data
+    """
+    # Initialize the output
+    data = list()
+
+    # Generate sine data
+    for i in range(no):
+        # Initialize each time-series
+        temp = list()
+        # For each feature
+        for k in range(dim):
+            # Randomly drawn frequency and phase
+            freq = np.random.uniform(0, 0.1)
+            phase = np.random.uniform(0, 0.1)
+
+            # Generate sine signal based on the drawn frequency and phase
+            temp_data = [np.sin(freq * j + phase) for j in range(seq_len)]
+            temp.append(temp_data)
+
+        # Align row/column
+        temp = np.transpose(np.asarray(temp))
+        # Normalize to [0,1]
+        temp = (temp + 1) * 0.5
+        # Stack the generated data
+        data.append(temp)
+
+    return data
+
+
+def real_data_loading(data_name, seq_len):
+    """Load and preprocess real-world data.
+
+    Args:
+      - data_name: stock or energy
+      - seq_len: sequence length
+
+    Returns:
+      - data: preprocessed data.
+    """
+    assert data_name in ['stock', 'energy', 'metro']
+
+    if data_name == 'stock':
+        ori_data = np.loadtxt('./data/short_range/stock_data.csv', delimiter=",", skiprows=1)
+    elif data_name == 'energy':
+        ori_data = np.loadtxt('./data/short_range/energy_data.csv', delimiter=",", skiprows=1)
+    elif data_name == 'metro':
+        ori_data = np.loadtxt('./data/short_range/metro_data.csv', delimiter=",", skiprows=1)
+
+    # Flip the data to make chronological data
+    ori_data = ori_data[::-1]
+    # Normalize the data
+    ori_data = MinMaxScaler(ori_data)
+
+    # Preprocess the data
+    temp_data = []
+    # Cut data by sequence length
+    for i in range(0, len(ori_data) - seq_len):
+        _x = ori_data[i:i + seq_len]
+        temp_data.append(_x)
+
+    # Mix the data (to make it similar to i.i.d)
+    idx = np.random.permutation(len(temp_data))
+    data = []
+    for i in range(len(temp_data)):
+        data.append(temp_data[idx[i]])
+
+    return data
+
 #
 # def gen_dataloader(args):
 #     if args.dataset == 'sine':
@@ -278,11 +285,6 @@
 #         return len(self.samples)
 
 
-import numpy as np
-from torch.utils.data import DataLoader, Dataset, Subset
-import torch
-import importlib
-from torch.utils.data import ConcatDataset
 
 
 def normalize_to_neg_one_to_one(x):
